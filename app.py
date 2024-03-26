ef7c678c53e145f5185cc5cf1bb6dd56643e55af
Charles Laurent Noble <120020015+ryuzen6@users.noreply.github.com>
2024-03-26 23:09:58 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import pprint
+import google.generativeai as palm
+
+palm.configure(api_key='AIzaSyBSX1CACqI6S9OBscb7TT08INLWiw7deL0')
+
+models = [m for m in palm.list_models() if 'generateText' in m.supported_generation_methods]
+model = models[0].name
+
+# Streamlit app
+st.title("BUG TRACKER")
+
+# User inputs
+input_code = st.text_input("Show me your code")
+error = st.text_input("Whats the error message")
+
+prompt = """
+I have coded a specific program and
+I seem to have got an error while coding it
+
+CODE:\n"""+input_code+"""\nERROR:\n"""+error+"""
+
+Can you give an error Explanation?
+Can you Suggest possible Fixes?
+Can you Give the corrected code?
+"""
+
+completion = palm.generate_text(
+    model=model,
+    prompt=prompt,
+    temperature=0,
+    # The maximum length of the response
+    max_output_tokens=800,
+)
+
+#display it to screen
+output = completion.result
+
+if (st.button("Debug")):
+    st.write(output)
